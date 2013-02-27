83daf02a8ee028138dd349aa98fb4514c2f4e72c
Michal Stekrt <michal@is76.org>
2013-02-27 01:55:21 +0100

    fix #28 n)

--- application/src/main/resources/CompendiumNG.bat
+++ application/src/main/resources/CompendiumNG.bat
@@ -1,3 +1,3 @@
 @echo off
 cd ${INSTALL_PATH}
-start /b javaw -Xmx512m -Dlog4j.configuration=file:${user.setting.dir}/log4j.properties com.compendium.ProjectCompendium %1 %2 %3 %4 %5 %6 %7 %8 %9
+start /b javaw -Xmx512m -Dlog4j.configuration=file:$USER_HOME/log4j.properties -jar CompendiumNG-jar-with-dependencies.jar  %1 %2 %3 %4 %5 %6 %7 %8 %9
--- application/src/main/resources/CompendiumNG.sh
+++ application/src/main/resources/CompendiumNG.sh
@@ -1,3 +1,2 @@
 cd %{INSTALL_PATH}
-# java -Xmx512m -Dlog4j.configuration=file:%{user.setting.dir}/log4j.properties com.compendium.ProjectCompendium %1 %2 %3 %4 %5 %6 %7 %8 %9
-java -Xmx512m -Dlog4j.configuration=file:$HOME/.compendiumng/log4j.properties -jar CompendiumNG-jar-with-dependencies.jar "$@"
+java -Xmx512m -Dlog4j.configuration=file:%USER_HOME/.compendiumng/log4j.properties -jar CompendiumNG-jar-with-dependencies.jar "$@"
