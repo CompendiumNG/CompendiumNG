6cf4366e14fbf46374e5e2501f19d050d6b0a6ed
Michal Stekrt <michal@is76.org>
2013-10-13 22:11:45 +0200

    fix start scripts

--- application/src/main/resources/CompendiumNG-portable.sh
+++ application/src/main/resources/CompendiumNG-portable.sh
@@ -1,2 +1,2 @@
 cd %{INSTALL_PATH}
-java -Dlogback.configurationFile="%{INSTALL_PATH}/logback.xml.default" -Xmx512m -jar CompendiumNG.jar "$@"
+javaw -Dlogback.configurationFile="%{INSTALL_PATH}/logback.xml.default" -Dcompendiumng.config.dir="%{INSTALL_PATH}/config" -Xmx512m -jar CompendiumNG.jar "$@"
