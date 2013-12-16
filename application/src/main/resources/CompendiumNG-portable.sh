64d3f21cc5d3efb57a714cae790df32eb385f490
Michal Stekrt <michal@is76.org>
2013-12-16 01:31:08 +0100

    rollback of #204 fix

--- application/src/main/resources/CompendiumNG-portable.sh
+++ application/src/main/resources/CompendiumNG-portable.sh
@@ -1,2 +1,2 @@
 cd %{INSTALL_PATH}
-javaw -Dlogback.configurationFile="%{INSTALL_PATH}/logback.xml.default" -Xdock:name="Compendium NG" -Dcompendiumng.config.dir="%{INSTALL_PATH}/config" -Xmx512m -jar CompendiumNG.jar "$@"
+javaw -Dlogback.configurationFile="%{INSTALL_PATH}/logback.xml" -Dcompendiumng.config.dir="%{INSTALL_PATH}/config" -Xmx512m -jar CompendiumNG.jar "$@"
--- application/src/main/resources/CompendiumNG.sh
+++ application/src/main/resources/CompendiumNG.sh
@@ -1,2 +1,2 @@
 cd %{INSTALL_PATH}
-java -Dlogback.configurationFile="%{INSTALL_PATH}/logback.xml.default" -Xdock:name="Compendium NG" -Dcompendiumng.config.dir=${HOME}/compendiumng_config -Ddir.data=${HOME}/compendiumng_data -Xmx512m -jar CompendiumNG.jar "$@"
+java -Dlogback.configurationFile="%{INSTALL_PATH}/logback.xml" -Dcompendiumng.config.dir=${HOME}/compendiumng_config -Ddir.data=${HOME}/compendiumng_data -Xmx512m -jar CompendiumNG.jar "$@"
