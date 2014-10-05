985f4755796510b194b4d9cdaf0d0428ba4106ce
Michal Stekrt <michal@is76.org>
2014-10-05 23:37:11 +0200

    add some memory tests

---
+++ cngx/src/test/java/org/compendiumng/cngx/MainFactory.java
@@ -0,0 +1,15 @@
+package org.compendiumng.cngx;
+
+/**
+ * Created by maiklos on 5.10.14.
+ */
+public class MainFactory {
+	public static org.compendiumng.cngx.memory.IMemory GetMemory() {
+		assert (false);
+		return null;
+	}
+
+	public static org.compendiumng.cngx.memory.IMemory GetMemory(String s) {
+		return null;
+	}
+}
